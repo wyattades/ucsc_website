ad20cb45b72be85e69cf16ac62322262fa488fb0
Wyatt Ades <wyattades@gmail.com>
2018-05-29 19:18:54 -0700

    Finished week08

--- art101/week08/index.js
+++ art101/week08/index.js
@@ -1,19 +1,25 @@
 
+// Tracery story grammar
 const storyBook = {
-  adv: ['undeniably','indescribably','marginally','unexpectedly','crazily','weirdly','unfortunately','unsurprisingly'],  
-  adj: ['large','big','lengthy','purple','sexy','corrupt','addicting','kickass','horrifying','gorgeous','beautiful','comfortable','miniature','obedient','salty','spicy'],
-  plural: ['gorillas','zookeepers','male models','circus people','candy wrappers','lawyers','mice','Atheists','mouse traps','lemonade','vodka','churches','molecules'],
-  singular: ['The President','Zack Effron','Tom Brady','Daffy Duck','The Queen','Earth','your mom','A Badly Burnt Albanian Boy',],
-  _subject: ['#singular#','#plural#'],
-  accels: ['sucks at','excels at'],
-  action: ['yoga','elephant parades','pull-ups'],
-  layout: ['Who knew #subject# could be so #adv# #adj#!?'],
-  story: ['#[subject:#_subject#]layout#'],
+  adv: ['undeniably','indescribably','marginally','unexpectedly','crazily','weirdly',
+        'unfortunately','unsurprisingly'],  
+  adj: ['large','big','lengthy','prickly','sexy','corrupt','addicting','kickass','horrifying',
+        'gorgeous','beautiful','comfortable','miniature','obedient','salty','spicy'],
+  subject: ['a gorilla','a zookeeper','a male model','a circus person','my dad\'s lawyer',
+            'a mouse','my atheist neighbor','lemonade','vodka','the Church','the molecule',
+            'The President','Zack Efron','Tom Cruz','Daffy Duck','The Queen','Earth',
+            'your mom','a Badly Burnt Albanian Boy','Albie the Racist Dragon','grilled corn'],
+  story: ['Who knew #subject# could be so #adv# #adj#!?',
+          'I\'ve heard that #subject# is #adv# #adj#.',
+          '#adj.capitalize#, #adj#, and #adj# are 3 words I\'d use to describe #subject#.'],
 };
 
 $(() => {
+  // Generate grammar once on page load
   const grammer = tracery.createGrammar(storyBook);
 
+  // When the #generate button is clicked, set the text in #output to a randomized
+  // output of the grammer
   const $output = $('#output');
   $('#generate').click(() => {
     $output.text(grammer.flatten('#story#'));
